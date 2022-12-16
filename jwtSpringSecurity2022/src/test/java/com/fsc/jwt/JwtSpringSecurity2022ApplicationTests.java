573ec6f9bdc2b0e7156967b9a9d9cc74e2d1aa23
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-12-16 01:14:22 -0300

    implemetado models

---
+++ jwtSpringSecurity2022/src/test/java/com/fsc/jwt/JwtSpringSecurity2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsc.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurity2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
